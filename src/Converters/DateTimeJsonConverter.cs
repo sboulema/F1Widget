311c084176e08f317ec4998f2fb5fe7e7dd6008f
sboulema <sboulema@gmail.com>
2024-11-04 09:33:33 +0100

    Return empty string instead of null to fix read error in KWGT

--- src/Converters/DateTimeJsonConverter.cs
+++ src/Converters/DateTimeJsonConverter.cs
@@ -4,12 +4,12 @@ using System.Globalization;
 
 namespace F1Widget.Converters;
 
-public class DateTimeJsonConverter : JsonConverter<DateTime>
+public class DateTimeJsonConverter : JsonConverter<DateTime?>
 {
     private static readonly CultureInfo Culture = CultureInfo.InvariantCulture;
     private const string Format = "dd MMM HH:mm";
 
-    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (!DateTime.TryParseExact(reader.GetString(), Format, Culture, DateTimeStyles.None, out var dateTime))
         {
@@ -19,8 +19,10 @@ public class DateTimeJsonConverter : JsonConverter<DateTime>
         return dateTime;
     }
 
-    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
     {
-        writer.WriteStringValue(value.ToString("dd MMM HH:mm"));
+        writer.WriteStringValue(value == null ? string.Empty : value.Value.ToString(Format));
     }
+    
+    public override bool HandleNull => true;
 }
